dc280d431852dd36410769bd3e4820f5d81af203
Agus-Silva-CPFS <agussilva23.work@gmail.com>
2025-06-04 15:20:46 -0300

    Se creo un componente para separar las secciones

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar/Navbar.tsx';
+import Separador from './components/Separador/Separador.tsx';
 import Inicio from './sections/Inicio/Inicio.tsx';
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <Inicio></Inicio>
+      <Separador></Separador>
     </div>
   );
 }
---
+++ src/components/Separador/Separador.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Separador/Separador.css';
+
+const Separador = () => {
+    return (
+        <div className="ctn-triangulo">
+            <div className="borde-triangulo"></div>
+            <div className="triangulo"></div>
+        </div>
+    );
+};
+export default Separador;
